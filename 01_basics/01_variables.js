d636ade240380d8dcde49108f26db2ee677be8e0
bargepanu16 <2750_pranali@katalystindia.org>
2025-04-14 16:29:48 +0000

    new

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId=23;
+let accountEmail="bfgdt@gmail.com";
+var accountPassword="123455";
+accountCity="Pune";
+let accountState;
+//accountId=2; not allowed
+accountEmail="hfdhs@.com";
+
+/*
+prefer not to use var
+because of issue in functionl and block scope
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity]);
