b78db1ef17de3b2e5f7db49b4ecf59d476b73d7f
bargepanu16 <2750_pranali@katalystindia.org>
2025-04-15 16:32:52 +0000

    Datatype

---
+++ 01_basics/02_Datatype.js
@@ -0,0 +1,18 @@
+"use strict"//treat all the javascript code as a newer version
+//alert(3+3); we are using node.js not browser
+
+console.log("Pranali");
+console.log(3+3);
+
+/*
+number ->2 power 53
+bigint
+string =""
+boolean-> true or false
+null standaloan value
+undefined->
+symbol->unique
+*/
+let age=23;
+let isLoogedin=true;
+let state;
