345469be39a7266726c3ab02a448504a681f8f34
bargepanu16 <2750_pranali@katalystindia.org>
2025-05-15 14:26:49 +0000

    object use

--- 01_basics/03_typeconvertion.js
+++ 01_basics/03_typeconvertion.js
@@ -2,3 +2,12 @@ let score="33";
 let type=Number(score);
 console.log(type);
  console.log(typeof score);
+
+ let MyObject={
+    email:"bae02@gmail.com",
+    upi:34567
+ }
+ let obj2=MyObject;
+obj2.email="Hello.com";
+ console.log(obj2.email);
+ console.log(MyObject.email);
