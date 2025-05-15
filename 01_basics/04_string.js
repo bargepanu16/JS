748761b6488b6b6266bdc1284c7602f63c6d8655
bargepanu16 <2750_pranali@katalystindia.org>
2025-05-15 16:05:43 +0000

    new

---
+++ 01_basics/04_string.js
@@ -0,0 +1,5 @@
+let name="Pranali";
+let amount=234;
+console.log(name+" "+amount);
+//use backtics  to concat data this is called string interpolation
+console.log(`I am ${name}. total ${amount}`);
