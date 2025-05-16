823dde34c3b242ab6557119dcf90c7ec65647876
bargepanu16 <2750_pranali@katalystindia.org>
2025-05-16 18:26:17 +0000

    time

--- 01_basics/06_dateTime.js
+++ 01_basics/06_dateTime.js
@@ -1,5 +1,12 @@
-let MyDate=new Date();
-console.log(MyDate);
-console.log(MyDate.toString());
-console.log(MyDate.toLocaleString());
-console.log(MyDate.toDateString());
+// let MyDate=new Date();
+// console.log(MyDate);
+// console.log(MyDate.toString());
+// console.log(MyDate.toLocaleString());
+// console.log(MyDate.toDateString());
+
+let createdDate=new Date(2025,0,3,5,3);
+//let createdDate=new Date("04-16-2025");//MM-DD-YY
+console.log(createdDate.toLocaleDateString());
+
+let timeStamp=Date.now();
+
