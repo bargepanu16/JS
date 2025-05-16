e1484eee939dbcc9e9caa168bea67717bd82de77
bargepanu16 <2750_pranali@katalystindia.org>
2025-05-16 20:25:13 +0000

    note

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,4 @@
+//In JavaScript, arrays are dynamic and versatile objects used to store multiple values in a single variable. 
+// They can hold any type of data — numbers, strings, objects, even other arrays.
+
+
