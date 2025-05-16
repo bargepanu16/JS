e13c4a7d7c8f9446469b82508f24da3f6f69167b
bargepanu16 <2750_pranali@katalystindia.org>
2025-05-16 15:57:06 +0000

    replace methode in string

--- 01_basics/04_string.js
+++ 01_basics/04_string.js
@@ -10,4 +10,13 @@ console.log(gameName.length);
 console.log(gameName.__proto__);
 
 console.log(gameName.charAt(2));
+console.log(gameName.indexOf('a'));
 console.log(gameName.toUpperCase());
+
+const newString=gameName.slice(-7,4);
+console.log(newString);
+
+let url="hello.20%.com";
+console.log(url.replace('20%','-'));
+
+
