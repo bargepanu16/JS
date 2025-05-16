b8034c57baf70acd60c12555165c29e83a84923f
bargepanu16 <2750_pranali@katalystindia.org>
2025-05-16 09:29:07 +0000

    strings

--- 01_basics/04_string.js
+++ 01_basics/04_string.js
@@ -3,3 +3,11 @@ let amount=234;
 console.log(name+" "+amount);
 //use backtics  to concat data this is called string interpolation
 console.log(`I am ${name}. total ${amount}`);
+
+const gameName=new String("Pranali");
+console.log(gameName[0]);
+console.log(gameName.length);
+console.log(gameName.__proto__);
+
+console.log(gameName.charAt(2));
+console.log(gameName.toUpperCase());
