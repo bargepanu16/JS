9cd9f4d19ddd699f7135cfada98363c6a69a1ae2
bargepanu16 <2750_pranali@katalystindia.org>
2025-05-16 20:50:56 +0000

    array

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -2,3 +2,37 @@
 // They can hold any type of data — numbers, strings, objects, even other arrays.
 
 
+//Array
+
+let myArray=[0,1,2,3,4,5];
+// let Myname=["Pranav","Sayli"];
+
+// let myArray2=new Array(1,2,3,4,6,8)
+// console.log(myArray[0]);
+
+// //methodes in array
+// console.log(`length of array is:- ${myArray.length}`);
+
+// myArray.push(6);
+// console.log(myArray);
+// console.log(`length of array after push is:- ${myArray.length}`);
+
+// myArray.pop();
+// myArray.pop();
+// console.log(`length of array after pop is:- ${myArray.length}`);
+
+// console.log(myArray);
+
+// myArray.unshift(9);
+// console.log(myArray);
+
+// myArray.shift();
+
+
+console.log(myArray.includes(9));
+console.log(myArray);
+
+const newarr=myArray.join();//Adds all the elements of an array into a string, separated by the specified separator string.
+
+
+console.log(newarr);
