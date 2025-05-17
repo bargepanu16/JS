8f91ab8b1705694ed337b69170323a12f2ebb8af
bargepanu16 <2750_pranali@katalystindia.org>
2025-05-17 11:25:06 +0000

    console.log(Array.from({name:"Pranali"}))

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -13,3 +13,13 @@ const dc=["superman","flash"];
 const all_new_heros=[...Marvales,...dc];
 console.log("After using spread operator (...firstarray,...secondarray) output is :-");
 console.log(all_new_heros);
+
+const array=[1,2,[3,4],5,6,[7,[8,9],44],67];
+
+const real_array=array.flat(Infinity)
+console.log(array);
+console.log(real_array);
+
+console.log(Array.isArray("Pranali"));
+console.log(Array.from("Pranali"));
+console.log(Array.from({name:"Pranali"}));//interesting case OUTPUT=> [] here we have to specify that by using key or value which we are using to make array
