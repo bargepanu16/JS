d8011d24cef770ef14d20f16993012bfd1b3a8b2
bargepanu16 <2750_pranali@katalystindia.org>
2025-05-17 13:59:47 +0000

    nested objects

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -2,4 +2,20 @@
 
 //previously we work with object literals
 
-const ten
+const tenderUser={};
+tenderUser.id="123abc";
+tenderUser.name="hitesh";
+tenderUser.email="hitesh123@.com";
+console.log(tenderUser);
+
+const regualuser={
+    email:"hello123@.com",
+    fullname:{
+        userfullname:{
+            firstname:"hiteshji",
+            lastname:"chopra"
+        }
+    }
+}
+
+console.log(regualuser.fullname.userfullname.firstname)
