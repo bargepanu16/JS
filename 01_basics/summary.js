b70917dd77b9fb37185a18a169a4999f6a82d0c9
bargepanu16 <2750_pranali@katalystindia.org>
2025-07-29 19:23:19 +0000

    var is not used because of block scop

---
+++ 01_basics/summary.js
@@ -0,0 +1,6 @@
+//primitive
+//7 types : string ,Number, Boolean,null,nudefined,Symbol,Bigint
+
+/* What does dynamically typed mean?
+In a dynamically typed language, you do not need to declare 
+a variable's type. The type is determined at runtime, based on the value assigned.*/
