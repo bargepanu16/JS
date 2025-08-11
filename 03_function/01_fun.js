5525bc8aad5a249cad368584a8f880623cf81fe7
bargepanu16 <2750_pranali@katalystindia.org>
2025-08-11 20:20:24 +0000

    arrow function

--- 03_function/01_fun.js
+++ 03_function/01_fun.js
@@ -1,7 +1,13 @@
+addTwoNum(4,7)
 function addTwoNum(num1,num2)
 {
     console.log(num1+num2);
     return num1+num2;
 }
-let result=addTwoNum(4,7);
-console.log(result);
+//let result=addTwoNum(4,7);
+//console.log(result);
+//fun2(50); ###############ReferenceError: Cannot access 'fun2' before initialization
+let fun2= function(num){
+    console.log(num);
+}
+fun2(50);
